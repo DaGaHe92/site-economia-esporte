9b2aef5f4499e471863ffbf4050bdd652295042b
DaGaHe92 <danielrodriguesdasilva1720@gmail.com>
2025-07-29 00:49:41 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Site Funcionando!</h1>
-      <p>Se você está vendo isso, deu tudo certo 🚀</p>
+      <h1>Olá, mundo! Este é meu site na Vercel!</h1>
     </div>
-  );
+  )
 }
